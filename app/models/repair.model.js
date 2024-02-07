6bc2002e8904da7f7bc3906ee2728f8e524b1164
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-06 20:20:37 -0600

    fix(repair model): fixed date DATE discrepancy

--- app/models/repair.model.js
+++ app/models/repair.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, Sequelize) => {
         allowNull: true,
       },
       date: {
-        type: Sequelize.date,
+        type: Sequelize.DATE,
         allowNull: false,
       },
       problems: {

8018602084ad9dc40e768ce438b8bbe4eecf9c7f
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-06 20:16:51 -0600

    fix(renovation model): fixed INT INTEGER discrepancy and added column metadata

--- app/models/renovation.model.js
+++ app/models/renovation.model.js
@@ -1,16 +1,21 @@
 module.exports = (sequelize, Sequelize) => {
     const Renovation = sequelize.define("renovation", {
         id:{
-            type: Sequelize.INT
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
         },
         date: {
-            type: Sequelize.date
+            type: Sequelize.date,
+            allowNull: false,
         },
         cost: {
-            type: Sequelize.INT
+            type: Sequelize.INTEGER,
+            allowNull: true,
         },
         description:{
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false,
         }
 
     });

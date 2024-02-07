12cf2a727b07bdde85258cdfb0873c0c710d0bd8
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-06 20:24:10 -0600

    fix(models): fixing metadata discrepancies

--- app/models/repair.model.js
+++ app/models/repair.model.js
@@ -3,7 +3,6 @@ module.exports = (sequelize, Sequelize) => {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        allowNull: false,
         primaryKey: true
       },
       notes: {
--- app/models/room.model.js
+++ app/models/room.model.js
@@ -2,9 +2,12 @@ module.exports = (sequelize, Sequelize) =>{
     const Room = sequelize.define("room", {
         id: {
             type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
         },
         roomNum : {
             type: Sequelize.STRING(15),
+            allowNull: false,
         }
     });
     return Room;

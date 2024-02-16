4ac851126159f1cececcf2c44d46d62771467642
trinajkenyon <trina.kenyon@eagles.oc.edu>
2024-02-16 13:58:34 -0600

    Fix: Updated model route to match all others

--- app/routes/model.routes.js
+++ app/routes/model.routes.js
@@ -21,6 +21,6 @@ module.exports = (app) => {
     router.delete("/", models.deleteAll);
   
     // Attach the router to the base path
-    app.use("/asset-t4/models", router);
+    app.use("/asset-t4/model", router);
   };
   

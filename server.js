6fd0d92bf158a764c2e641a4867295093cbab56e
Stonerunner24 <eli.coyle1@gmail.com>
2024-02-17 21:37:11 -0600

    fix(auth): made auth work somewhat. Need to double check with merged backend

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-//require("./app/routes/auth.routes.js")(app);
+require("./app/routes/auth.routes.js")(app);
 require("./app/routes/item.routes.js")(app);
 require("./app/routes/type.routes.js")(app);
 require("./app/routes/category.routes.js")(app);
